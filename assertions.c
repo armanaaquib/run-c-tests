14919abca1f44cf7068bc2415d9650e89cd64c88
Aaquib Equbal <44823431+armanaaquib@users.noreply.github.com>
2020-05-21 11:16:46 +0530

    Added: two more assertions methods
    assert_int_array_equal
    assert_linked_list_equal

--- assertions.c
+++ assertions.c
@@ -11,3 +11,53 @@ void assert_int_equal(int actual, int expected, Test_ptr test)
     sprintf(test->error, "Expected %d Actual %d\n", expected, actual);
   }
 }
+
+void assert_int_array_equal(int *array_1, int *array_2, int length, Test_ptr test)
+{
+  if(test->status == False) return;
+
+  for(int i = 0; i < length; i++)
+  {
+    int actual = array_1[i];
+    int expected = array_2[i];
+
+    if(!(actual == expected))
+    {
+      test->status = False;
+      sprintf(test->error, "Expected %d Actual %d\n", expected, actual);
+      break;
+    }
+  }
+}
+
+void assert_linked_list_equal(LinkedList_ptr ll_1, LinkedList_ptr ll_2, Matcher is_el_equal, Test_ptr test)
+{
+  if(test->status == False) return;
+
+  Bool is_equal = (ll_1->length == ll_2->length);
+
+  if(!is_equal)
+  {
+    test->status = False;
+    sprintf(test->error, "Expected length %d Actual length %d\n", ll_2->length, ll_1->length);
+    return;
+  }
+
+  Node_ptr p_walk_1 = ll_1->first;
+  Node_ptr p_walk_2 = ll_2->first;
+
+  while(p_walk_1 != NULL)
+  {
+    is_equal = (*is_el_equal)(p_walk_1->element, p_walk_2->element);
+
+    if(!is_equal)
+    {
+      test->status = False;
+      sprintf(test->error, "Elements are not equal");
+      break;
+    }
+
+    p_walk_1 = p_walk_1->next;
+    p_walk_2 = p_walk_2->next;
+  }
+}
--- assertions.h
+++ assertions.h
@@ -4,5 +4,34 @@
 #define __ASSERTIONS_H_
 
 void assert_int_equal(int, int, Test_ptr);
+void assert_int_array_equal(int *, int *, int, Test_ptr);
+
+///* if you have already made these typedefs in your actual program,
+//simply comment it and include your program header file above
+
+typedef void *Element;
+
+typedef struct node
+{
+  Element element;
+  struct node *prev;
+  struct node *next;
+} Node;
+
+typedef Node *Node_ptr;
+
+typedef struct
+{
+  Node_ptr first;
+  Node_ptr last;
+  int length;
+} Linked_list;
+
+typedef Linked_list *LinkedList_ptr;
+
+typedef Bool (*Matcher)(Element, Element);
+//*/
+
+void assert_linked_list_equal(LinkedList_ptr, LinkedList_ptr, Matcher, Test_ptr);
 
 #endif
